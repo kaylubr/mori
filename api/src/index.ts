84fe08a909e3818802e8fc404f53ac41d641b1c1
kaylubr <kylebreyes092005@gmail.com>
2026-08-14 23:22:59 +0800

    chore: change utility to lib and install zod

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app.js"
 import config from "./config/index.js"
-import logger from "./utils/logger.js"
+import logger from "./lib/logger.js"
 
 app.listen(config.PORT, () => {
   logger.info(`Server listening to PORT: ${config.PORT}`)
